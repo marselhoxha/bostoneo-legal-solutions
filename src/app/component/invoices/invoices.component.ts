7c95a473b9d0fb9e7f0a50f2602873e44dc876d6
Marsel Hoxha <marsel.hox@gmail.com>
2024-08-21 21:22:09 -0400

    Implemented Invoice feature

--- src/app/component/invoices/invoices.component.ts
+++ src/app/component/invoices/invoices.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { EventType, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Observable, BehaviorSubject, map, startWith, catchError, of } from 'rxjs';
 import { DataState } from 'src/app/enum/datastate.enum';
-import { CustomHttpResponse, Page, Profile } from 'src/app/interface/appstates';
-import { Customer } from 'src/app/interface/customer';
+import { CustomHttpResponse, Page } from 'src/app/interface/appstates';
+import { Invoice } from 'src/app/interface/invoice';
 import { State } from 'src/app/interface/state';
 import { User } from 'src/app/interface/user';
 import { CustomerService } from 'src/app/service/customer.service';
-import { UserService } from 'src/app/service/user.service';
 
 @Component({
-  selector: 'app-customers',
-  templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css']
+  selector: 'app-invoices',
+  templateUrl: './invoices.component.html',
+  styleUrls: ['./invoices.component.css']
 })
-export class CustomersComponent implements OnInit {
-  customersState$: Observable<State<CustomHttpResponse<Page<Customer> & User>>>;
-  private dataSubject = new BehaviorSubject<CustomHttpResponse<Page<Customer> & User>>(null);
+export class InvoicesComponent  implements OnInit {
+  invoicesState$: Observable<State<CustomHttpResponse<Page<Invoice> & User>>>;
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<Page<Invoice> & User>>(null);
   private isLoadingSubject = new BehaviorSubject<boolean>(false);
   isLoading$ = this.isLoadingSubject.asObservable();
   private currentPageSubject = new BehaviorSubject<number>(0);
@@ -29,7 +27,7 @@ export class CustomersComponent implements OnInit {
   constructor(private router: Router, private customerService: CustomerService) { }
 
   ngOnInit(): void {
-    this.customersState$ = this.customerService.searchCustomers$()
+    this.invoicesState$ = this.customerService.invoices$()
       .pipe(
         map(response => {
           console.log(response);
@@ -43,24 +41,8 @@ export class CustomersComponent implements OnInit {
       )
   }
 
-  searchCustomers(searchForm: NgForm): void {
-    this.currentPageSubject.next(0);
-    this.customersState$ = this.customerService.searchCustomers$(searchForm.value.name)
-      .pipe(
-        map(response => {
-          console.log(response);
-          this.dataSubject.next(response);
-          return { dataState: DataState.LOADED, appData: response };
-        }),
-        startWith({ dataState: DataState.LOADED, appData: this.dataSubject.value }),
-        catchError((error: string) => {
-          return of({ dataState: DataState.ERROR, error })
-        })
-      )
-  }
-
-  goToPage(pageNumber?: number, name?: string): void {
-    this.customersState$ = this.customerService.searchCustomers$(name, pageNumber)
+  goToPage(pageNumber?: number): void {
+    this.invoicesState$ = this.customerService.invoices$(pageNumber)
       .pipe(
         map(response => {
           console.log(response);
@@ -75,12 +57,8 @@ export class CustomersComponent implements OnInit {
       )
   }
 
-  goToNextOrPreviousPage(direction?: string, name?: string): void {
-    this.goToPage(direction === 'forward' ? this.currentPageSubject.value + 1 : this.currentPageSubject.value - 1, name);
-  }
-
-  selectCustomer(customer: Customer): void {
-    this.router.navigate([`/customers/${customer.id}`]);
+  goToNextOrPreviousPage(direction?: string): void {
+    this.goToPage(direction === 'forward' ? this.currentPageSubject.value + 1 : this.currentPageSubject.value - 1);
   }
 
 }
